5fd0a3cc070dd0d4317bad8ad2d839dea56b309e
Kaspar Martin Suursalu <kasparsu@gmail.com>
2023-03-27 09:26:33 +0300

    add puppeteer

--- main.js
+++ main.js
@@ -1,38 +1,30 @@
-import axios from "axios";
-import fs from 'fs';
-import * as cheerio from 'cheerio';
-const delay = ms => new Promise(resolve => setTimeout(resolve, ms));
-for(let i=405; i>402;i--){
-    try {
-    let data;
-    if(!fs.existsSync('./cache')){
-        fs.mkdirSync('./cache');
-    }
-    if (!fs.existsSync(`./cache/${i}.html`)) {
-        console.log('LIVE REQUEST!!!!');
-        await delay(5000);
-        
+import puppeteer from 'puppeteer';
 
-      
-            let res = await axios.get(`https://xkcd.com/${i}/`);
-            data = res.data;
-            //console.log(res.data);
-            fs.writeFileSync(`./cache/${i}.html`, data);
-   
-    } else {
-        console.log('CACHED REQUEST!!!!');
-        data = fs.readFileSync(`./cache/${i}.html`);
-    }
-    let $ = cheerio.load(data);
-    let imgTag = $('#comic>img');
-    console.log(imgTag.attr('src'));
-    console.log(imgTag.attr('title'));
-    console.log(imgTag.attr('alt'));
-    } catch(e){
-        console.log(e);
-    }
-    
-}
-// let data = await axios.post("https://9gag.com/v1/feed-posts/type/home?after=eyJmZWVkSWQiOiJmNDcyM2Q1MTQwMTNiZjc2YWYxMzI4M2E2Y2Y4ZmFiZSIsIm9yZGVySWQiOjE5OTk5OTYwLCJvZmZzZXQiOjQwfQ%3D%3D");
-// let body = await data.json();
-// console.log(body);
+(async () => {
+    const browser = await puppeteer.launch();
+    const page = await browser.newPage();
+  
+    await page.goto('https://developer.chrome.com/');
+  
+    // Set screen size
+    await page.setViewport({width: 1080, height: 1024});
+  
+    // Type into search box
+    await page.type('.search-box__input', 'automate beyond recorder');
+  
+    // Wait and click on first result
+    const searchResultSelector = '.search-box__link';
+    await page.waitForSelector(searchResultSelector);
+    await page.click(searchResultSelector);
+  
+    // Locate the full title with a unique string
+    const textSelector = await page.waitForSelector(
+      'text/Customize and automate'
+    );
+    const fullTitle = await textSelector.evaluate(el => el.textContent);
+  
+    // Print the full title
+    console.log('The title of this blog post is "%s".', fullTitle);
+  
+    await browser.close();
+  })();
